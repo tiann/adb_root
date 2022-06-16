2b6f1ec23215e6874036f9ed6fb510a47dc06789
tiann <twsxtd@gmail.com>
2022-06-16 17:55:57 +0800

    README

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ fi
 
 (cd adbd_helper && ndk-build)
 
+cp README.md magisk_module/
 cp adbd_helper/libs/arm64-v8a/adbd magisk_module/system/apex/com.android.adbd/bin/adbd
 cp adbd_helper/libs/arm64-v8a/libadb_root_helper.so magisk_module/system/lib64/
 
